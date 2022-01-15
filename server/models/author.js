a9dc8bc6d960b74bb5c41793d5314b31c4c1d173
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-15 23:26:18 +0530

    define author schema

--- server/models/author.js
+++ server/models/author.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: false,
+    },
+});
+
+module.exports = mongoose.model("Authors", authorSchema);
