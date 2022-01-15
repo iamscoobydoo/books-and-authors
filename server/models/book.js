1df60b2da43596f7b58efa595bc4b127f4eb1740
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-15 23:24:46 +0530

    define book schema

--- server/models/book.js
+++ server/models/book.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: false,
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Authors",
+        required: false,
+    },
+});
+
+module.exports = mongoose.model("Books", bookSchema);
