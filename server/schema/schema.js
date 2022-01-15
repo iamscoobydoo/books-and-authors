cb894d277ac78c72b06f9debe123eed3b5e34ef2
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-15 23:35:22 +0530

    create addAuthor mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,9 @@ const graphql = require("graphql");
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLList } =
     graphql;
 
+const Book = require("../models/book");
+const Author = require("../models/author");
+
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
@@ -63,6 +66,31 @@ const RootQuery = new GraphQLObjectType({
     },
 });
 
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {
+                    type: GraphQLString,
+                },
+                age: {
+                    type: GraphQLInt,
+                },
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age,
+                });
+                return author.save();
+            },
+        },
+    },
+});
+
 module.exports = new GraphQLSchema({
     query: RootQuery,
+    mutation: Mutation,
 });
