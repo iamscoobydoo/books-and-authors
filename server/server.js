255883ac247ee849b81a9a32844a20e6aa87ae18
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-15 23:03:39 +0530

    connect to DB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
 const schema = require("./schema/schema");
 const app = express();
+
+mongoose.connect(
+    ` mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kckor.mongodb.net/libraryData?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+    console.log("Connected to DB!");
+});
 
 app.use(
     "/graphql",
