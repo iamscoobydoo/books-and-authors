d50803f79984fbdf921bd3e2828d9aecf09e4c43
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-21 01:56:02 +0530

    handle states

--- client/src/Components/AddBook.js
+++ client/src/Components/AddBook.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import { getAuthorsQuery } from "../Queries/Queries";
 
 function AddBook() {
+    const [bookDetails, setBookDetails] = useState({
+        name: "",
+        genre: "",
+        authorId: "",
+    });
+
+    const changeHandler = (e) => {
+        setBookDetails({ ...bookDetails, [e.target.name]: e.target.value });
+    };
+
+    const submitBookDetails = (e) => {
+        e.preventDefault();
+        console.log(bookDetails);
+    };
+
     const { loading, error, data } = useQuery(getAuthorsQuery);
 
     if (loading) return <p>Loading Author...</p>;
@@ -11,20 +26,20 @@ function AddBook() {
 
     return (
         <>
-            <form id='add-book'>
+            <form id='add-book' onSubmit={submitBookDetails}>
                 <div className='field'>
                     <label>Book Name:</label>
-                    <input type='text' />
+                    <input type='text' name='name' onChange={changeHandler} />
                 </div>
 
                 <div className='field'>
                     <label>Genre:</label>
-                    <input type='text' />
+                    <input type='text' name='genre' onChange={changeHandler} />
                 </div>
 
                 <div className='field'>
                     <label>Author:</label>
-                    <select>
+                    <select name='authorId' onChange={changeHandler}>
                         <option> Select Author</option>
                         {data.authors.map((author) => {
                             return (
