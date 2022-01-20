889de8b894309f1a3b3f6dd607856035a4d6ee22
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-21 01:02:33 +0530

    add useQuery

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
 
 function BookList() {
+    const { loading, error, data } = useQuery(getBooksQuery);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error </p>;
     return (
         <>
             <ul id='book-list'>
