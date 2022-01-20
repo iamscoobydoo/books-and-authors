11400a124ac2ab2fc24ed49e4d8f5201ef48573e
iamscoobydoo <arka.bhattacharya33@gmail.com>
2022-01-21 01:22:12 +0530

    init AddBook component

---
+++ client/src/Components/AddBook.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`;
+
+function AddBook() {
+    const { loading, error, data } = useQuery(getAuthorsQuery);
+
+    if (loading) return <p>Loading Author...</p>;
+    if (error) return <p>Error </p>;
+
+    return (
+        <>
+            <form id='add-book'>
+                <div className='field'>
+                    <label>Book Name:</label>
+                    <input type={text} />
+                </div>
+
+                <div className='field'>
+                    <label>Genre:</label>
+                    <input type={text} />
+                </div>
+
+                <div className='field'>
+                    <label>Author:</label>
+                    <select>
+                        <option> Select Author</option>
+                    </select>
+                </div>
+
+                <button>+</button>
+            </form>
+        </>
+    );
+}
+
+export default AddBook;
